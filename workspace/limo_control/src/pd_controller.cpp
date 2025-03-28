e18be609e3e152b3dc7945de0cd873e5d3fee409
Alyssa D'Souza <alyssa.dsouza63@gmail.com>
2025-03-27 21:53:50 -0400

    Added proportional controller

--- workspace/limo_control/src/pd_controller.cpp
+++ workspace/limo_control/src/pd_controller.cpp
@@ -18,12 +18,38 @@ public:
   }
 
 private:
+  bool first_callback = false;
+  double offset = 2.53;
+  double k_p = 1;
+  double original_pose_y = 0;
+  double goal_y = 0;
+  double prev_err = 0;
+
+  double pose_y = 0;
+  double del_t = 0.01;
+
+  double calculate_vel() {
+    // v_x = k_p * e_x(t) + k_d * de_x(t)/dt
+    double err = goal_y - pose_y;
+    double vel = k_p * err;
+    prev_err = err;
+    std::cout << err << std::endl;
+    return vel;
+  }
+
   void topic_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
   {
-    // Print the received message to the console
-    std::cout << "Received: " << msg->pose.position.x << std::endl;
+    double msg_pose_y = msg->pose.position.y; 
     geometry_msgs::msg::Twist drive_cmd;
-    drive_cmd.linear.x = 0.25;
+    if (!first_callback) {
+      original_pose_y = msg_pose_y;
+      goal_y = original_pose_y + offset;
+      first_callback = true;
+    }
+    // Print the received message to the console
+    std::cout << "Received: " << msg->pose.position.x << ", " << msg->pose.position.y << std::endl;
+    pose_y = msg_pose_y;
+    drive_cmd.linear.x = calculate_vel();
     publisher_->publish(drive_cmd);
     std::cout << "Drive: " << drive_cmd.linear.x << std::endl; 
   }
